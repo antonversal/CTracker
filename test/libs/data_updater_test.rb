fce235bd1d7cbe2e0ce12c0f94b25d4941c197a3
Anton Versal <ant.ver@gmail.com>
2013-11-12 12:24:41 +0200

    refactored data_uploader

--- test/libs/data_updater_test.rb
+++ test/libs/data_updater_test.rb
@@ -2,15 +2,30 @@ require 'test_helper'
 require 'data_uploader/data_updater'
 
 class DataUpdaterTest < ActiveSupport::TestCase
-  test "parse_response returns correct hash structure" do
-    response = { :get_currencies_response => { :get_currencies_result => "<NewDataSet>\n  <Table>\n    <Name>Afghanistan, Islamic State of</Name>\n    <CountryCode>af</CountryCode>\n    <Currency>Afghani</Currency>\n    <CurrencyCode>AFA</CurrencyCode>\n  </Table>\n  </NewDataSet>" } }
-    expected = {
-      :countries  => [{ :name => "Afghanistan, Islamic State of", :code => "af"   }],
-      :currencies => [{ :name => "Afghani", :code => "AFA", :country_id => "af" }]
-    }
+  setup do
+    result = <<-res
+      <NewDataSet>
+        <Table>
+          <Name>Afghanistan, Islamic State of</Name>
+          <CountryCode>af</CountryCode>
+          <Currency>Afghani</Currency>
+          <CurrencyCode>AFA</CurrencyCode>
+        </Table>
+      </NewDataSet>
+    res
+    response = { get_currencies_response:
+                 { get_currencies_result: result } }
+    @data_uploader = DataUpdater.new
+    Savon::Client.any_instance.stubs(:call).returns(response)
+  end
 
-    DataUpdater.send(:public, :parse_response)
-    
-    assert expected, DataUpdater.instance.parse_response(response)
+  test "#update adds countries" do
+    @data_uploader.update
+    assert_equal "Afghanistan, Islamic State of", Country.find_by!(code: "af").name
+  end
+
+  test "#update adds currency" do
+    @data_uploader.update
+    assert_equal "Afghani", Currency.find_by!(code: "AFA").name
   end
 end
