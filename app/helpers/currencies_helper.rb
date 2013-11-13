9f6e33705f87856d0764318eb18240a9676a06cf
Anton Versal <ant.ver@gmail.com>
2013-11-13 16:48:37 +0200

    collected checkbox tag

--- app/helpers/currencies_helper.rb
+++ app/helpers/currencies_helper.rb
@@ -1,2 +1,9 @@
 module CurrenciesHelper
+  def collected_checkbox_tag(currency)
+    check_box_tag(:visited,
+                  currency.country_id,
+                  currency.is_collected_by?(current_user),
+                  class: "visit",
+                  data: {url: countries_visited_path, country_id: currency.country_id})
+  end
 end
