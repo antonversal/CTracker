7f948fa7d925a5fb0e63ff4747d3d04e9da2166e
Anton Versal <ant.ver@gmail.com>
2013-11-13 15:12:01 +0200

    visit checkbox to helper

--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -1,2 +1,9 @@
 module CountriesHelper
+  def visited_checkbox_tag(country)
+  check_box_tag(:visited,
+      country.id,
+      country.is_visited_by?(current_user),
+          class: "visit",
+      data: {url: countries_visited_path, country_id: country.id})
+  end
 end
