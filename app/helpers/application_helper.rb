68fa023807a2aa9cb454443447fcd2edae5000a1
Anton Versal <ant.ver@gmail.com>
2013-11-13 16:48:20 +0200

    pie tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def pie_tag(data = {})
+    content_tag(:div, nil,
+                {id: "visited_countries_chart",
+                 class: "simple_pie_chart",
+                 width: '300px',
+                 height: '100px',
+                 data: data})
+  end
 end
