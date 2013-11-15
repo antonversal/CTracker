e278d633794ee9ca50b5b18c61343364425d6c64
Anton Versal <ant.ver@gmail.com>
2013-11-15 11:57:12 +0200

    fixed js script which probably sometimes raises testing errors

--- app/assets/javascripts/visit.js
+++ app/assets/javascripts/visit.js
@@ -1,61 +1,61 @@
 $(function () {
-  var changeVisitAllState = function(){
+  var changeVisitAllState = function () {
     var checked_count = $('input.visit:checkbox:checked').length;
     var all_count = $('input.visit:checkbox').length;
-     if (checked_count === all_count)
-     {
-       $("input.visit-all").prop('checked', true);
-     }
-      else
-     {
-       $("input.visit-all").prop('checked', false);
-     }
+    if (checked_count === all_count) {
+      $("input.visit-all").prop('checked', true);
+    }
+    else {
+      $("input.visit-all").prop('checked', false);
+    }
   };
 
-  var create = function(url, country_ids){
-    $.post(url, {country_ids: country_ids}).success(Charts.lineChart.update);
+  var create = function (url, country_ids) {
+    $.post(url, {country_ids: country_ids}).success(function () {
+      Charts.simplePieChart.add_visited(country_ids.length);
+      Charts.lineChart.update();
+    });
   };
 
-  var destroy = function(url, country_ids){
+  var destroy = function (url, country_ids) {
     $.ajax({
       url: url,
       type: 'PUT',
       data: {
         country_ids: country_ids
       }
-    }).success(Charts.lineChart.update);
+    }).success(function () {
+        Charts.simplePieChart.remove_visited(country_ids.length);
+        Charts.lineChart.update();
+      });
   };
 
-  $('table.visit-table').on("change", "input.visit", function(){
+  $('table.visit-table').on("change", "input.visit", function () {
     var url = $(this).data("url");
     var country_id = $(this).data("country-id");
-    if($(this).is(':checked')){
+    if ($(this).is(':checked')) {
       create(url, [country_id]);
-      Charts.simplePieChart.add_visited(1);
     } else {
       destroy(url, [country_id]);
-      Charts.simplePieChart.remove_visited(1);
     }
     changeVisitAllState();
   });
 
-  $('table.visit-table').on("change", "input.visit-all", function(){
+  $('table.visit-table').on("change", "input.visit-all", function () {
     var url = $(this).data("url");
-    if($(this).is(':checked')){
+    if ($(this).is(':checked')) {
       var checkboxes = $('input.visit:checkbox:not(:checked)');
-      var country_ids = checkboxes.map(function(){
+      var country_ids = checkboxes.map(function () {
         return $(this).data("country-id")
       }).get();
-      Charts.simplePieChart.add_visited(country_ids.length);
       checkboxes.prop('checked', true);
       if (country_ids.length > 0) create(url, country_ids);
     } else {
       var checkboxes = $('input.visit:checkbox:checked');
-      var country_ids = checkboxes.map(function(){
+      var country_ids = checkboxes.map(function () {
         return $(this).data("country-id")
       }).get();
       checkboxes.prop('checked', false);
-      Charts.simplePieChart.remove_visited(country_ids.length);
       if (country_ids.length > 0) destroy(url, country_ids);
     }
     changeVisitAllState();
