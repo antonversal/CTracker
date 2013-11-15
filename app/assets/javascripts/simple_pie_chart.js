895057fcf38d123422aa090b6b11dd54d39c0c8c
Anton Versal <ant.ver@gmail.com>
2013-11-15 08:56:19 +0200

    refactored simple pie chart

--- app/assets/javascripts/simple_pie_chart.js
+++ app/assets/javascripts/simple_pie_chart.js
@@ -1,70 +1,42 @@
-// SimplePieChart
-// Author: Bradley J. Spaulding
-// Created On: 2011-04-25
+var SimplePieChart = function (root_element) {
+  var root = root_element,
+      visited_label = $(root).data("visited-label"),
+      not_visited_label = $(root).data("not-visited-label"),
+      visited_count = $(root).data("visited-count"),
+      not_visited_count = $(root).data("not-visited-count");
 
-var simplePieChart;
-var SimplePieChart = {};
+  var self = this;
 
-SimplePieChart.initialize = function(root_element) { return new ViewController(root_element, {
-  initialize: function() {
-    this.width  = parseInt( this.root.getAttribute('width') );
-    this.height = parseInt( this.root.getAttribute('height') );
+    this.render = function() {
+      var options = {
+        title: 'My Progress',
+        is3D: true
+      };
+      self.chart.draw(this.data(), options);
+    };
 
-    this.visited_label = $(this.root).data("visited-label");
-    this.not_visited_label = $(this.root).data("not-visited-label");
-    this.visited_count = $(this.root).data("visited-count");
-    this.not_visited_count = $(this.root).data("not-visited-count");
+    this.data = function() {
+      return google.visualization.arrayToDataTable([
+        ['Status', 'Counts'],
+        [visited_label, visited_count],
+        [not_visited_label, not_visited_count]
+      ]);
+    };
 
-    this.render();
-  },
+    this.add_visited = function (count) {
+      visited_count = visited_count + count;
+      not_visited_count = not_visited_count - count;
+      this.render();
+    },
 
-  render: function() {
-    $(this.root).html('');
+    this.remove_visited = function (count) {
+      visited_count = visited_count - count;
+      not_visited_count = not_visited_count + count;
+      this.render();
+    }
 
-    var img = document.createElement('img');
-    img.setAttribute('width', this.width);
-    img.setAttribute('height', this.height);
-    img.setAttribute('src', this.image_url());
-
-    this.root.appendChild(img);
-  },  
-
-  image_url: function() {
-    var url = "http://chart.googleapis.com/chart?"
-    url += "cht=p3"
-    url += "&chs=" + this.width + 'x' + this.height
-    url += "&chco=0000FF"
-    
-    url += "&chd=t:" + this.values().join();
-    url += "&chdl=" + this.labels().join('|');
-
-    return url;
-  },
-
-  values: function() {
-    return [this.visited_count, this.not_visited_count];
-  },
-
-  labels: function() {
-    return [this.visited_label, this.not_visited_label];
-  },
-
-  add_visited: function(count){
-    this.visited_count = this.visited_count + count;
-    this.not_visited_count = this.not_visited_count - count;
-    this.render();
-  },
-
-  remove_visited: function(count){
-    this.visited_count = this.visited_count - count;
-    this.not_visited_count = this.not_visited_count + count;
-    this.render();
-  }
-
-})};
-
-$(document).ready(function() {
-  $('.simple_pie_chart').each(function() {
-    simplePieChart = SimplePieChart.initialize(this);
-  });
-});
+  this.constructor = function () {
+    self.chart = new google.visualization.PieChart(root);
+    self.render();
+  }();
+};
